a5c8e2ef94571225f587405eb3cbae03bf351d20
Santiago Franco <ktns0930@gmail.com>
2023-01-07 23:21:43 -0500

    Is ready to use.

--- main.py
+++ main.py
@@ -4,6 +4,39 @@ from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def password_generator():
+    # Password Generator Project
+    import random
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    entry_password.delete(0, END)
+    entry_password.insert(0, password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -65,7 +98,7 @@ entry_username.insert(0, "example@gmail.com")
 entry_password = Entry(width=35)
 entry_password.grid(column=1, row=3, columnspan=2)
 
-btn_generate_pass = Button(text="Generate Password", width=30)
+btn_generate_pass = Button(text="Generate Password", width=30, command=password_generator)
 btn_generate_pass.grid(column=1, row=4, columnspan=2)
 
 btn_add = Button(text="Add", width=25, command=save_password)
